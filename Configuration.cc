e592711b26ddd823d999e638ed704b9fe1afe019
Vincent Berthoux <twinside@gmail.com>
2010-03-04 17:32:39 +0100

    Commiting two missing files

---
+++ Configuration.cc
@@ -0,0 +1,13 @@
+#include "Configuration.h"
+
+namespace ViWm
+{
+    Configuration::Configuration(const char *filename)
+    {
+    }
+
+    Configuration::~Configuration()
+    {
+    }
+}
+
---
+++ Configuration.h
@@ -0,0 +1,17 @@
+#ifndef __CONFIGURATION_H__
+#define __CONFIGURATION_H__
+
+namespace ViWm
+{
+    class Configuration
+    {
+    public:
+        Configuration( const char *filename );
+        ~Configuration();
+        
+
+    private:
+    };
+}
+
+#endif /* __CONFIGURATION_H__ */
